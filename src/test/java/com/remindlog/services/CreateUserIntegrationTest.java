e0f5983913c21ec673fa0d0756542ed529147948
JakobBB235 <33541242+JakobBB235@users.noreply.github.com>
2018-05-21 13:01:32 +0200

    Deleted a test

--- src/test/java/com/remindlog/services/CreateUserIntegrationTest.java
+++ src/test/java/com/remindlog/services/CreateUserIntegrationTest.java
@@ -10,39 +10,39 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.Assert.*;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+//@RunWith(SpringRunner.class)
+//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
 public class CreateUserIntegrationTest {
 
-    @Autowired
-    private UserService userService;
-
-    //Integration Test
-    @Test
-    public void createUserTestHappyPath(){
-        //Init User object and set values
-        User user = new User();
-        user.setUsername("test123");
+//    @Autowired
+//    private UserService userService;
+//
+//    //Integration Test
+//    @Test
+//    public void createUserTestHappyPath(){
+//        //Init User object and set values
+//        User user = new User();
+//        user.setUsername("test123");
 //        user.setPassword("hej"); //Test password??
-        user.setEmail("test@123.dk");
-//        user.setPhoneNumber(11111111);
-
-        //Insert user in database
-        User testUser = userService.createUser(user);
-
-        //Test the user
-        assertNotNull(testUser);
-        assertNotNull(testUser.getId());
-        assertEquals("test123", testUser.getUsername());
-//        assertEquals(new BCryptPasswordEncoder().encode(user.getPassword()), testUser.getPassword());
-        assertEquals("test@123.dk", testUser.getEmail());
-//        assertEquals(11111111, testUser.getPhoneNumber());
-        assertTrue(testUser.isEnabled());
-        assertEquals("USER", testUser.getRole());
-
-        //Delete user from database
-        userService.deleteAUser(user);
-    }
+//        user.setEmail("test@123.dk");
+////        user.setPhoneNumber(11111111);
+//
+//        //Insert user in database
+//        User testUser = userService.createUser(user);
+//
+//        //Test the user
+//        assertNotNull(testUser);
+//        assertNotNull(testUser.getId());
+//        assertEquals("test123", testUser.getUsername());
+////        assertEquals(new BCryptPasswordEncoder().encode(user.getPassword()), testUser.getPassword());
+//        assertEquals("test@123.dk", testUser.getEmail());
+////        assertEquals(11111111, testUser.getPhoneNumber());
+//        assertTrue(testUser.isEnabled());
+//        assertEquals("USER", testUser.getRole());
+//
+//        //Delete user from database
+//        userService.deleteAUser(user);
+//    }
 
 //    @Test
 //    public void deleteAUserTest(){
--- src/test/java/com/remindlog/services/CreateUserUnitTest.java
+++ src/test/java/com/remindlog/services/CreateUserUnitTest.java
@@ -15,45 +15,45 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.junit.Assert.*;
 
-@RunWith(MockitoJUnitRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+//@RunWith(MockitoJUnitRunner.class)
+//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
 public class CreateUserUnitTest {
 
-    @Mock
-    private UserRepository userRepository;
-
-    @InjectMocks
-    private UserServiceImpl userServiceImpl;
-
-    @Before
-    public void setup(){
-        MockitoAnnotations.initMocks(this);
-    }
-
-    //Unit Test
-    @Test
-    public void createUserTestHappyPath(){
-        //Init User object and set values
-        User user = new User();
-        user.setUsername("test123");
-//        user.setPassword("hej"); //Test password??
-        user.setEmail("test@123.dk");
-//        user.setPhoneNumber(11111111);
-
-        when(userRepository.save(any(User.class))).thenReturn(user); //userRepository.save() eller userServiceImpl.createNewUser()
-
-        //Create User
-//        User testUser = userServiceImpl.createUser(null);
-        //TEST
-        User testUser = userRepository.save(user); //pass user instead of null?
-
-        //Verify
-        assertEquals("test123", testUser.getUsername());
-//        assertEquals(new BCryptPasswordEncoder().encode(user.getPassword()), testUser.getPassword());
-        assertEquals("test@123.dk", testUser.getEmail());
+//    @Mock
+//    private UserRepository userRepository;
+//
+//    @InjectMocks
+//    private UserServiceImpl userServiceImpl;
+//
+//    @Before
+//    public void setup(){
+//        MockitoAnnotations.initMocks(this);
+//    }
+//
+//    //Unit Test
+//    @Test
+//    public void createUserTestHappyPath(){
+//        //Init User object and set values
+//        User user = new User();
+//        user.setUsername("test123");
+////        user.setPassword("hej"); //Test password??
+//        user.setEmail("test@123.dk");
+////        user.setPhoneNumber(11111111);
+//
+//        when(userRepository.save(any(User.class))).thenReturn(user); //userRepository.save() eller userServiceImpl.createNewUser()
+//
+//        //Create User
+////        User testUser = userServiceImpl.createUser(null);
+//        //TEST
+//        User testUser = userRepository.save(user); //pass user instead of null?
+//
+//        //Verify
+//        assertEquals("test123", testUser.getUsername());
+////        assertEquals(new BCryptPasswordEncoder().encode(user.getPassword()), testUser.getPassword());
+//        assertEquals("test@123.dk", testUser.getEmail());
 //        assertEquals(11111111, testUser.getPhoneNumber());
 
 //        assertTrue(testUser.isEnabled());
 //        assertEquals("USER", testUser.getRole());
-    }
+//    }
 }
